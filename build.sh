e35ad9ad215db6db88960be37abea5821a1040aa
Pzqqt <821026875@qq.com>
2019-08-28 18:21:27 +0800

    build.sh: try built with llvm 8.0.6

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ else
 	echo "Build for Pie firmware"
 fi
 
+[ "$1" == "--llvm" ] && use_llvm=true || use_llvm=false
+
 yellow='\033[0;33m'
 white='\033[0m'
 red='\033[0;31m'
@@ -14,6 +16,11 @@ gre='\e[0;32m'
 ZIMG=./out/arch/arm64/boot/Image.gz-dtb
 
 export LOCALVERSION=-v1.9
+if $use_llvm; then
+	export LOCALVERSION=${LOCALVERSION}-llvm
+else
+	export LOCALVERSION=${LOCALVERSION}-clang
+fi
 
 rm -f $ZIMG
 Start=$(date +"%s")
@@ -21,14 +28,19 @@ Start=$(date +"%s")
 export ARCH=arm64
 export SUBARCH=arm64
 export HEADER_ARCH=arm64
-export CLANG_PATH=/home/pzqqt/bin/android_prebuilts_clang_host_linux-x86_clang-5799447
+if $use_llvm; then
+	export CLANG_PATH=/home/pzqqt/bin/snapdragon-llvm-8.0.6-linux64/toolchains/llvm-Snapdragon_LLVM_for_Android_8.0/prebuilt/linux-x86_64
+else
+	export CLANG_PATH=/home/pzqqt/bin/android_prebuilts_clang_host_linux-x86_clang-5799447
+fi
 export KBUILD_COMPILER_STRING=$($CLANG_PATH/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 
 export KBUILD_BUILD_HOST="lenovo"
 export KBUILD_BUILD_USER="pzqqt"
 
-make mrproper O=out && \
-make whyred-perf_defconfig O=out && \
+make mrproper O=out
+make whyred-perf_defconfig O=out
+$use_llvm && set CONFIG_LLVM_POLLY=y
 make -j6 \
 	O=out \
 	CC="ccache $CLANG_PATH/bin/clang" \
