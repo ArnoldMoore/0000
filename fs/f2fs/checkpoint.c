43b9b89b7ef0c7cdec47cfff1187557378c26371
Pzqqt <821026875@qq.com>
2019-08-28 17:51:12 +0800

    Revert "f2fs: ioctl for removing a range from F2FS"
    This reverts commit 35f17d924fb046503135381c8ef13ae130451349.

--- fs/f2fs/checkpoint.c
+++ fs/f2fs/checkpoint.c
@@ -1300,8 +1300,7 @@ static void update_ckpt_flags(struct f2fs_sb_info *sbi, struct cp_control *cpc)
 	else
 		__clear_ckpt_flags(ckpt, CP_ORPHAN_PRESENT_FLAG);
 
-	if (is_sbi_flag_set(sbi, SBI_NEED_FSCK) ||
-		is_sbi_flag_set(sbi, SBI_IS_RESIZEFS))
+	if (is_sbi_flag_set(sbi, SBI_NEED_FSCK))
 		__set_ckpt_flags(ckpt, CP_FSCK_FLAG);
 
 	if (is_sbi_flag_set(sbi, SBI_CP_DISABLED))
